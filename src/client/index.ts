93135f56346e19288baa0eaf68cbc6e4a755937f
Sandor Teleki <sandorteleki1@gmail.com>
2026-06-05 21:28:45 +0200

    feat: client repl

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,8 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, getInput, printClientHelp, printQuit, commandStatus } from "../internal/gamelogic/gamelogic.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/queue.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
@@ -22,11 +25,40 @@ async function main() {
   );
   console.log(`Queue ${queueName} declared and bound.`);
 
-  process.on("SIGINT", async () => {
-    console.log("Shutting down...");
-    await conn.close();
-    process.exit(0);
-  });
+  const gs = new GameState(username);
+
+  while (true) {
+    const words = await getInput();
+    if (words.length === 0) {
+      continue;
+    }
+
+    const command = words[0];
+
+    try {
+      if (command === "spawn") {
+        commandSpawn(gs, words);
+      } else if (command === "move") {
+        commandMove(gs, words);
+      } else if (command === "status") {
+        await commandStatus(gs);
+      } else if (command === "help") {
+        printClientHelp();
+      } else if (command === "spam") {
+        console.log("Spamming not allowed yet!");
+      } else if (command === "quit") {
+        printQuit();
+        break;
+      } else {
+        console.log(`I don't understand the command: ${command}`);
+      }
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  }
+
+  await conn.close();
+  process.exit(0);
 }
 
 main().catch((err) => {
