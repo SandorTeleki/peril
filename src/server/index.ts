a61a5e9cc37f07e9b1b8a6b64f09fecdd004ffbd
Sandor Teleki <sandorteleki1@gmail.com>
2026-06-05 12:28:27 +0200

    feat: amqp connected

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection to RabbitMQ successful!");
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    await conn.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
