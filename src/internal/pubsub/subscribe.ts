01e42489914631fb31e367b0964c6ff2565b7070
Sandor Teleki <sandorteleki1@gmail.com>
2026-06-05 21:47:51 +0200

    feat: ack and nack

--- src/internal/pubsub/subscribe.ts
+++ src/internal/pubsub/subscribe.ts
@@ -1,13 +1,19 @@
 import amqp from "amqplib";
 import { declareAndBind, SimpleQueueType } from "./queue.js";
 
+export enum AckType {
+  Ack,
+  NackRequeue,
+  NackDiscard,
+}
+
 export async function subscribeJSON<T>(
   conn: amqp.ChannelModel,
   exchange: string,
   queueName: string,
   key: string,
   queueType: SimpleQueueType,
-  handler: (data: T) => void,
+  handler: (data: T) => AckType,
 ): Promise<void> {
   const [ch, queue] = await declareAndBind(conn, exchange, queueName, key, queueType);
 
@@ -16,7 +22,21 @@ export async function subscribeJSON<T>(
       return;
     }
     const data = JSON.parse(message.content.toString()) as T;
-    handler(data);
-    ch.ack(message);
+    const ackType = handler(data);
+
+    switch (ackType) {
+      case AckType.Ack:
+        console.log("Ack");
+        ch.ack(message);
+        break;
+      case AckType.NackRequeue:
+        console.log("NackRequeue");
+        ch.nack(message, false, true);
+        break;
+      case AckType.NackDiscard:
+        console.log("NackDiscard");
+        ch.nack(message, false, false);
+        break;
+    }
   });
 }
