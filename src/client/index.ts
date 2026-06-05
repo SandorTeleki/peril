d73881aef0d741f07359cd6f9af172d814fc1949
Sandor Teleki <sandorteleki1@gmail.com>
2026-06-05 21:21:16 +0200

    feat: add queues

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,32 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/queue.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection to RabbitMQ successful!");
+
+  const username = await clientWelcome();
+
+  const queueName = `pause.${username}`;
+  const [ch, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    queueName,
+    PauseKey,
+    SimpleQueueType.Transient,
+  );
+  console.log(`Queue ${queueName} declared and bound.`);
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    await conn.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
---
+++ src/internal/pubsub/queue.ts
@@ -0,0 +1,31 @@
+import amqp from "amqplib";
+import type { Channel } from "amqplib";
+
+export enum SimpleQueueType {
+  Durable,
+  Transient,
+}
+
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[Channel, amqp.Replies.AssertQueue]> {
+  const ch = await conn.createChannel();
+
+  const durable = queueType === SimpleQueueType.Durable;
+  const autoDelete = queueType === SimpleQueueType.Transient;
+  const exclusive = queueType === SimpleQueueType.Transient;
+
+  const queue = await ch.assertQueue(queueName, {
+    durable,
+    autoDelete,
+    exclusive,
+  });
+
+  await ch.bindQueue(queueName, exchange, key);
+
+  return [ch, queue];
+}
